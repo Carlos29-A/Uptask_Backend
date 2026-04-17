e6817f7a6e7d3e09fe4a171772c32aa0eefbcf4d
Carlos <caguilarvillanueva68@gmail.com>
2026-04-17 18:04:34 -0500

    Implement CRUD operations for projects in ProjectController and update project routes for validation

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -8,8 +8,7 @@ export class ProjectController {
         const project = new Project(req.body);
 
         try {
-
-
+            // Guardamos el proyecto en la base de datos
             await project.save();
             res.status(201).json('Proyecto creado correctamente');
 
@@ -20,7 +19,66 @@ export class ProjectController {
     }
     // Obtener todos los proyectos
     static getAllProjects = async (req: Request, res: Response) => {
-        res.send('GET Projects');
-    }
+        try {
 
+            // Buscamos todos los proyectos en la base de datos
+            const projects = await Project.find({});
+
+            res.status(200).json(projects);
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Error al obtener los proyectos' });
+        }
+    }
+    // Obtener un proyecto por su ID
+    static getProjectById = async (req: Request, res: Response) => {
+        const { id } = req.params;
+        try {
+            // Buscamos el proyecto por su ID en la base de datos
+            const project = await Project.findById(id);
+
+            if (!project) {
+                return res.status(404).json({ message: 'Proyecto no encontrado' });
+            }
+
+            res.status(200).json(project);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Error al obtener el proyecto' });
+        }
+    }
+    // Actualizar un proyecto por su ID
+    static updateProjectById = async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const { projectName, clientName, description } = req.body;
+        try {
+            // Buscamos el proyecto por su ID en la base de datos
+            const project = await Project.findByIdAndUpdate(id, { projectName, clientName, description }, { new: true });
+
+            if (!project) {
+                return res.status(404).json({ message: 'Proyecto no encontrado' });
+            }
+
+            res.status(200).json('Proyecto actualizado correctamente');
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Error al actualizar el proyecto' });
+        }
+    }
+    // Eliminar un proyecto por su ID
+    static deleteProjectById = async (req: Request, res: Response) => {
+        const { id } = req.params;
+        try {
+            // Buscamos el proyecto por su ID en la base de datos
+            const project = await Project.findByIdAndDelete(id);
+            if (!project) {
+                return res.status(404).json({ message: 'Proyecto no encontrado' });
+            }
+            res.status(200).json('Proyecto eliminado correctamente');
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Error al eliminar el proyecto' });
+        }
+    }
 }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
 import { handleInputErrors } from '../middleware';
 
@@ -19,6 +19,33 @@ router.post('/',
     ProjectController.createProject
 );
 router.get('/', ProjectController.getAllProjects);
+router.get('/:id',
+    param('id')
+        .isMongoId().withMessage('El ID no es válido'),
+    handleInputErrors,
+    ProjectController.getProjectById);
+
+router.put('/:id',
+    param('id')
+        .isMongoId().withMessage('El ID no es válido'),
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripción es obligatoria'),
+    handleInputErrors,
+    ProjectController.updateProjectById
+);
+
+router.delete('/:id',
+    param('id')
+        .isMongoId().withMessage('El ID no es válido'),
+    handleInputErrors,
+    ProjectController.deleteProjectById
+);
+
+
 
 // exportamos las rutas
 export default router;
