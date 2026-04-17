4bfd22d6409141811e3e45bb846f8b6d0048aa22
Carlos <caguilarvillanueva68@gmail.com>
2026-04-17 18:17:00 -0500

    feat: Add status field to Task model with predefined task statuses

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,10 +1,23 @@
 import mongoose, { Schema, Document, Types } from 'mongoose';
 
+
+
+const taskStatus = {
+    PENDING: 'pending',
+    ON_HOLD: 'onHold',
+    IN_PROGRESS: 'inProgress',
+    UNDER_REVIEW: 'underReview',
+    COMPLETED: 'completed',
+} as const;
+
+export type TaskStatus = typeof taskStatus[keyof typeof taskStatus];
+
 // para que typescript sepa que tipo de datos es la tarea
 export type TaskType = Document & {
     name: string;
     description: string;
     project: Types.ObjectId;
+    status: TaskStatus;
 }
 
 // definimos el esquema de la tarea 
@@ -22,6 +35,11 @@ export const TaskSchema = new Schema<TaskType>({
     project: {
         type: Types.ObjectId,
         ref: 'Project',
+    },
+    status: {
+        type: String,
+        enum: Object.values(taskStatus),
+        default: taskStatus.PENDING,
     }
 }, {
     timestamps: true,
