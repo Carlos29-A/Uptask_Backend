4082bfe094ec68fa2604fe867cf2bdfaa83a92b0
Carlos <caguilarvillanueva68@gmail.com>
2026-04-17 19:17:07 -0500

    feat: Implement task retrieval and update functionality with validation for project ownership

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from 'express';
 import Project from '../models/Project';
 import Task from '../models/Task';
+import { isValidObjectId } from 'mongoose';
 
 
 export class TaskController {
@@ -27,6 +28,45 @@ export class TaskController {
         } catch (error) {
             console.log(error);
             res.status(500).json({ message: 'Error al obtener las tareas' });
+        }
+    }
+
+    // Obtener una tarea por su ID
+    static getTaskById = async (req: Request, res: Response) => {
+        try {
+            const { taskId } = req.params;
+            const task = await Task.findById(taskId).populate('project');
+            if (!task) {
+                return res.status(404).json({ message: 'Tarea no encontrada' });
+            }
+            // Verificar si la tarea su proyecto es el mismo del proyecto que se está solicitando
+            if (task.project.toString() !== req.project._id.toString()) {
+                return res.status(403).json({ message: 'La tarea no pertenece al proyecto' });
+            }
+            res.status(200).json(task);
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Error al obtener la tarea' });
+        }
+    }
+
+    // Actualizar una tarea por su ID
+    static updateTaskById = async (req: Request, res: Response) => {
+        try {
+            const { taskId } = req.params;
+            const task = await Task.findByIdAndUpdate(taskId, req.body, { new: true }).populate('project');
+            if (!task) {
+                return res.status(404).json({ message: 'Tarea no encontrada' });
+            }
+            // Verificar si la tarea su proyecto es el mismo del proyecto que se está solicitando
+            if (task.project.toString() !== req.project._id.toString()) {
+                return res.status(403).json({ message: 'La tarea no pertenece al proyecto solicitado' });
+            }
+            res.status(200).json(task);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Error al actualizar la tarea' });
         }
     }
 }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -52,8 +52,9 @@ router.delete('/:id',
 
 
 // rutas para las tareas
+// Middleware para verificar si el proyecto existe
+router.param('projectId', projectExists);
 router.post('/:projectId/tasks',
-    projectExists,
     body('name')
         .notEmpty().withMessage('El nombre de la tarea es obligatorio'),
     body('description')
@@ -63,9 +64,28 @@ router.post('/:projectId/tasks',
 )
 // Obtener todas las tareas de un proyecto
 router.get('/:projectId/tasks',
-    projectExists,
     TaskController.getAllTasks
 )
+// Obtener una tarea por su ID
+router.get('/:projectId/tasks/:taskId',
+    param('taskId')
+        .isMongoId().withMessage('El ID de la tarea no es válido'),
+    handleInputErrors,
+    TaskController.getTaskById
+)
+
+// Actualizar una tarea por su ID
+router.put('/:projectId/tasks/:taskId',
+    param('taskId')
+        .isMongoId().withMessage('El ID de la tarea no es válido'),
+    body('name')
+        .notEmpty().withMessage('El nombre de la tarea es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripción de la tarea es obligatoria'),
+    handleInputErrors,
+    TaskController.updateTaskById
+)
+
 
 // exportamos las rutas
 export default router;
